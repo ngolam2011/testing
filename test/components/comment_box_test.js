f3ef551df27b78b0a1efd81824a9b01389c9c40c
Ngo Lam <lngo9@csc.com>
2018-03-06 18:46:32 +0700

    final

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,13 +3,36 @@ import CommentBox from '../../src/components/comment_box';
 
 
 describe('CommentBox', () => {
-  it('has a text area', () => {
-    const component = renderComponent(CommentBox);
+  let component;
+  beforeEach(() => {
+    component = renderComponent(CommentBox); 
+  });
 
+  it('has the correct class', () => {
+    expect(component).to.have.class('comment-box');
+  });
+
+  it('has a text area', () => {
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
+    expect(component.find('button')).to.exist;
+  });
+  
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
 
   });
 });
